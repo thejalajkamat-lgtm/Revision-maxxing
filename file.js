b19a02cd260786a27b09991a019e6e72075a1ca1
Jalaj Kamat <thejalajkamat@gmail.com>
2026-08-16 14:36:10 +0530

    http

--- file.js
+++ file.js
@@ -1,12 +1,24 @@
 const fs = require("fs");
 // fs.writeFileSync('./test.txt', 'Hello Whore');
 
-fs.writeFile('./test.txt', "Hello Chut", (err) => {});
-const result = fs.readFile('./contacts.txt', "utf-8", (err, result) => {
-    if(err) {
-        console.log("Error",err)
-    }else{
-        console.log("Result:", result)
-    }
-});
+// fs.writeFile('./test.txt', "Hello Chut", (err) => {});
+// const result = fs.readFile('./contacts.txt', "utf-8", (err, result) => {
+//     if(err) {
+//         console.log("Error",err)
+//     }else{
+//         console.log("Result:", result)
+//     }
+// });
+// console.log(result);
+
+
+// fs.appendFileSync("./test.txt", `${Date.now()}Hey There\n`);
+// fs.cpSync('./test.txt', './copy.txt');
+// fs.unlink("./copy.txt", (err) => {})
+
+// console.log(fs.statSync("./test.txt"));
+
+fs.writeFileSync("test.txt", "Hello World");
+fs.writeFile("test.txt", "Hello World Async", (err) => {});
+let result = fs.readFileSync("contacts.txt", "utf-8");
 console.log(result);
