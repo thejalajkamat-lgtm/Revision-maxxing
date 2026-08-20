be7d4e019f64b721b8b00eea2ced61d54a7c86e9
Jalaj Kamat <thejalajkamat@gmail.com>
2026-08-20 21:36:29 +0530

    hoes are horrible

--- http.js
+++ http.js
@@ -1,23 +1,15 @@
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
-const myServer = http.createServer( (req, res) => {
-    const log = `${Date.now()}: ${req.url}New Request Received\n`;
-    const myUrl = url.parse(req.url);
-    console.log(myUrl);
-    fs.appendFile("log.txt", log, (err, data) => {
-        switch(myUrl.pathname){
-            case '/':
-                res.end("Homepage");
-                break;
-            case '/about':
-                res.end("I am Jalaj Kamat");
-                break;
-            default:
-                res.end("404 Not Found")
-        }
-        res.end("Hello from Server ");
-    });
-});
-myServer.listen(8080, () => console.log("Server started"));
+const express = require("express");
+const app = express();
+
+app.get('/', (req,res) => {
+    return res.send("Hello From Home Page");
+})
+app.get('/about', (req,res) => {
+    return res.send(`Hello ${req.query.name}`);
+})
+app.get('/profile', (req,res) => {
+    return res.send(`Hello from profile page`);
+})
+app.listen(8080, () => console.log("Server started"));
 
